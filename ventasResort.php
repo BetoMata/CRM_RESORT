6630eb11934a67e51448317bccb245319c601a3b
Israel Pérez Mejía <israelpmejia@outlook.es>
2021-05-30 13:19:25 -0500

    Ventas

--- ventasResort.php
+++ ventasResort.php
@@ -44,9 +44,48 @@
 			print("<");
 			print("<br>");
 			print("<div ><img src='paquetes_img/".$Fila[0]."'><h3>".$Fila[1].$Fila[5]."</h3></div>");
+
+		print("
+		<form>
+			<label>Destino: ".$Fila[2]."</label><br>
+			<input type='hidden' name='destinoDato' maxlength='250' class='inputL' required><br>
+			<label>Salida: ".$Fila[3]."</label><br>
+			<input type='hidden' name='salidaDato' maxlength='250' class='inputL' required><br>
+			<label>Fecha de Llegada</label><br>
+			<input type='date' name='fechallegadaDato' class='inputL' required><br>	
+			<label>Fecha de Salida</label><br>
+			<input type='date' name='fechasalidaDato' class='inputL' required><br>
+			<label># Adultos</label><br>
+			<input type='number' name='adultosDato' min='1' max='100' maxlength='3' class='inputCh' required><br>
+			<label># Menores</label><br>
+			<input type='number' name='menoresDato' min='0' max='100' maxlength='3' class='inputCh' required><br>
+			<label>Edades menores</label><br>
+			<input type='text' placeholder='Ejemplo: 8, 12' maxlength='1'name='edadmenoresDato' class='inputCh' required><br>
+			<label>Nombre</label><br>
+			<input type='text' placeholder='Ejemplo: Juan Pérez' name='nombreDato' id='nombre' class='inputForm short' required><br>
+			<label>Tu correo electrónico</label><br>
+			<input type='email' name='correoDato' min='0' max='1' maxlength='1' class='inputCh' required><br>
+			<label>Tel / Whatsapp</label><br>
+			<input type='number' name='telDato' min='0' max='999' maxlength='3' class='inputCh' required><br>
+			<label>Precio</label>
+		<?php
+			$Adulto=$_POST['adultosDato'];
+			$Menor=$_POST['menoresDato'];
+			$Precio=$Fila[4] * $Adulto * $Menor;
+
+			print($Precio);
+		?>
+			<input type='number' name='menoresDato' min='0' max='100' maxlength='3' class='inputCh' required><br>
+			<input type='submit' name='submit' value='Guardar' class='btnEnviar'>
+		</form>
+		");
         };
-        
+
+		
+
         Cerrar($Con);
       ?>
+
+
 	</body>
 </html>

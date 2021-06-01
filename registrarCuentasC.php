9e4e2d8c04da88a0c00f4d8951450db6d8f1c9b2
Israel Pérez Mejía <israelpmejia@outlook.es>
2021-05-31 19:23:32 -0500

    Conexión Cuentas

--- registrarCuentasC.php
+++ registrarCuentasC.php
@@ -1,35 +1,71 @@
-<?php
-	$nombreDato=$_POST['nombreDato'];
-	$apellidoDato=$_POST['apellidoDato'];
-	$telefonoDato=$_POST['numeroDato'];
-	$correoDato=$_POST['correoDato'];
-	$usernameDato=$_POST['usernameDato'];
-	$contrasenaDato=$_POST['claveDato'];
-	$conf_contrasenaDato=$_POST['validaClave'];
-	
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  	<meta charset="utf-8">
+	<title>Registro Cuenta</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<link rel="stylesheet" href="css/swiper.min.css">
+	<link rel="stylesheet" href="css/estilos.css">
+</head>
+<body id="Contacto">
+	<a name="Inicio"></a>
+  	<header>
+    	<img src="img/logo.png">
+    	<nav class="menu">
+        	<ul>
+	          <li><a href="#svs">Inicio</a></li>
+	          <li><a href="#hatchback">Reservaciones</a></li>
+	          <li><a href="#coupes">Mis Viajes</a></li>
+	          <li><a href="contactoResort.html">Contacto</a></li>
+	          <li><a href="productos.html">Nosotros</a></li>
+        	</ul>
+      	</nav>
+  	</header>
+	<div class="mainContacto">
 
-include("ControladorBD.php");
+        <?php
+	        $nombreDato=$_POST['nombreDato'];
+			$apellidoDato=$_POST['apellidoDato'];
+			$telefonoDato=$_POST['numeroDato'];
+			$correoDato=$_POST['correoDato'];
+			$usernameDato=$_POST['usernameDato'];
+			$contrasenaDato=$_POST['claveDato'];
+			$conf_contrasenaDato=$_POST['validaClave'];
 
-	$Con = conectar();
-	$SQL = "INSERT INTO cuentas_clientes VALUES (default, '$nombreDato', '$apellidoDato', '$telefonoDato', '$correoDato', '$usernameDato', '$contrasenaDato', '1');";
-	$Cons = consultar($Con,$SQL);
+	        Print("Tu cuenta ha sido creada!"."<br>");
+            include("ControladorBD.php");
 
-	
-	
-	
-	
-	
-	//header('Location: catalogoResort.php');
-	
-	cerrar($Con);
-	
-?>
+	        $Con = conectar();
+	        $SQL = "INSERT INTO cuentas_clientes VALUES (default, '$nombreDato', '$apellidoDato', '$telefonoDato', '$correoDato', 
+			'$usernameDato', '$contrasenaDato', '1');";
+	        $Cons = consultar($Con,$SQL);
+	        cerrar($Con);
+        ?>
 
-<script src="registrado.js"></script>
+	</div>
 
 
-
-
+	<footer id="contacto">
+	    <div class="partFooter">
+	      <img src="img/logo.png" alt="">
+	    </div>
+	    <div class="partFooter">
+	      <h4>Hotel & Resort</h4>
+	      <a href="login.html">Inicio</a>
+	    </div>
+	    <div class="partFooter">
+	      <h4>Acerca de</h4>
+	      <a href="about.html">Hotel & Resort</a>
+	    </div>
+	    <div class="partFooter">
+	        <h4>Redes sociales</h4>
+	        <div class="social-media">
+	            <a href="#">f</a>
+	        </div>
+	    </div>
+	  </footer>
+</body>
+</html>
 
 
 
